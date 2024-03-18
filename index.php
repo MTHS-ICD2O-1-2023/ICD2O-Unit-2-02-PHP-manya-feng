4884d8822ef049646c8bda5ea85fff3366b5d1ad
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-03-18 18:38:04 +0000

    change style and words

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="The math program, in PHP" />
-    <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Manya" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/style.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
-    <link rel="manifest" href="site.webmanifest" />
-    <title>The math program,in PHP</title>
-  </head>
-  <body>
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="The math program, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Manya" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>The math program,in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
         <span class="mdl-layout-title">The math program, in PHP</span>
@@ -25,17 +27,16 @@
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/notebook-animation.svg" alt="laptop image" />
+        <img src="images/area_and_perimeter.png">
       </div>
       <div class="page-content">
-        <?php echo '<p>6 + 5 = ' . (6 + 5) . '</p>'; ?>
-        <?php echo '<p>7 - 3 =  ' . (7 - 3) . '</p>'; ?>
-        <?php echo '<p>3 + 4 × 2 = ' . (3 + 4 * 2) . '</p>'; ?>
-        <?php echo '<p>(4 ÷ 2) + 3 = ' . (4 / 2 + 3) . '</p>'; ?>
-        <?php echo '<p>5 + 2³ = ' . (5 + 2 ** 3) . '</p>'; ?>
+        <p>The dimensions of a rectangle are: 5 cm and 3 cm.</p>
+        <?php echo '<p>The area is : ' . (5 * 3) . ' cm² </p>'; ?>
+        <?php echo '<p>The preimeter is : ' . (2 * (5 + 3)) . ' cm </p>'; ?>
       </div>
       <br />
     </main>
-    </div>
-  </body>
+  </div>
+</body>
+
 </html>
